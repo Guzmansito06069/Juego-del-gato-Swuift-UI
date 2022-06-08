0e43393d9329876d111555a89d2ab5fa00b021a2
Edgar Alejandroguzman <alejandroguzman23221@gmail.com>
2022-06-08 15:50:45 -0500

    Initial Commit

---
+++ JuegoGato/JuegoGatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuegoGatoApp.swift
+//  JuegoGato
+//
+//  Created by Solidusystems on 08/06/22.
+//
+
+import SwiftUI
+
+@main
+struct JuegoGatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
